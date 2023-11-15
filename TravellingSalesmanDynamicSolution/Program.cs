91c96e225974caa79e1e08487550704bc943fc49
Lachlan Garrity <lachlangarrity@gmail.com>
2023-11-15 17:10:28 +1100

    init

---
+++ TravellingSalesmanDynamicSolution/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using TravellingSalesmanDynamicSolution;
+
+TravellingSalesmanDynamic travellingSalesman = new TravellingSalesmanDynamic();
+Console.WriteLine(travellingSalesman.testString());
+
---
+++ TravellingSalesmanDynamicSolution/TravellingSalesmanDynamic.cs
@@ -0,0 +1,9 @@
+﻿namespace TravellingSalesmanDynamicSolution;
+
+public class TravellingSalesmanDynamic
+{
+    public String testString()
+    {
+        return "TEST";
+    }
+}
